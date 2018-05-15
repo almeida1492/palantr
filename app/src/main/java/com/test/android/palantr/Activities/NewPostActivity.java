fd3546657a52906dff292d5f2787a5d2068ea961
Henrique de Almeida <henriquealmeida1492@gmail.com>
2018-05-15 15:36:37 -0300

    Implementa navegação entre main e novo post

--- app/src/main/java/com/test/android/palantr/Activities/NewPostActivity.java
+++ app/src/main/java/com/test/android/palantr/Activities/NewPostActivity.java
@@ -1,10 +1,57 @@
 package com.test.android.palantr.Activities;
 
+import android.graphics.PorterDuff;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.test.android.palantr.R;
 
 /**
  * Created by henriquedealmeida on 10/05/18.
  */
 
 public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.SRC_ATOP);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_post_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_pic:
+
+                return true;
+            case R.id.add_signature:
+
+                return true;
+            case R.id.add_topic:
+
+                return true;
+            case R.id.send_post:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
