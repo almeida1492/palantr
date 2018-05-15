aed95ae50e29f4b87b39f6a765d7f143717b3b9d
Henrique de Almeida <henriquealmeida1492@gmail.com>
2018-05-15 14:47:26 -0300

    Cria campos para edição em xml de novo post

--- app/src/main/java/com/test/android/palantr/Activities/NewPostActivity.java
+++ app/src/main/java/com/test/android/palantr/Activities/NewPostActivity.java
@@ -1,8 +1,10 @@
 package com.test.android.palantr.Activities;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by henriquedealmeida on 10/05/18.
  */
 
-public class NewPostActivity {
+public class NewPostActivity extends AppCompatActivity {
 }
